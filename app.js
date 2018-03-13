d31e8290ec694815df604798e410345c6d6c5beb
Javier Pérez Arteaga <surtich@gmail.com>
2018-03-13 17:23:08 +0100

    Added Error Handler service

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const helloComponent = require('./services/hello').API
+const errorManagement = require('errorManagement')
 
 console.log(`App is currently starting`)
 
 let app = express()
 
 app.use('/api/hello', helloComponent)
+errorManagement.handling.registerAndHandleAllErrors(app)
 
 module.exports = app
